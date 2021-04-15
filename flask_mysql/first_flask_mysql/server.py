517fe1be5ade5a6e51057bd711207de04d4d5c80
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-04-15 16:05:35 -0400

    CRUD initial commit

--- flask_mysql/first_flask_mysql/server.py
+++ flask_mysql/first_flask_mysql/server.py
@@ -1,32 +1,118 @@
 from flask import Flask, render_template, request, redirect
-from mysqlconnection import connectToMySQL # import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
 app = Flask(__name__)
+
+#!  initial landing page
 @app.route("/")
 def index():
-    mysql = connectToMySQL('users')# call the function, passing in the name of our db
-    users = mysql.query_db('SELECT * FROM users;') # call the query_db function, pass in the query as a string
-    print(users)
+    mysql = connectToMySQL('users')
+    users = mysql.query_db('SELECT * FROM users;')
     return render_template("index.html", all_friends = users)
 
+
+#!  create friend page
 @app.route("/create_friend")
 def add_friend_to_db():
-    return render_template("create.html")
-    
+    mysql = connectToMySQL('users')
+    users = mysql.query_db('SELECT * FROM users;')
+    # print(users.id)
+    return render_template("create.html", users = users[0])
 
-@app.route("/return_friend", methods = ["POST"])
-def return_add():
-    mysql = connectToMySQL("users")
 
+#!  new friend info edit and submit
+@app.route("/return_friend/<int:user_id>", methods = ["POST"])
+def return_add(user_id):
     query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());"
     data = {
+        'id': user_id,
         "first_name": request.form["first_name"],
         "last_name": request.form["last_name"],
         "email": request.form["email"]
     }
+    user = connectToMySQL("users").query_db(query,data)
+    print(user)
+    return redirect (f"/display_friend/{user_id}")
 
-    mysql.query_db(query, data)
+
+#!  update friend
+# @app.route("/update_friend/<int:user_id>", methods = ['POST'])
+# def update(user_id):
+#     query = "UPDATE users SET first_name = %(first_name)s , last_name = %(last_name)s, email = %(email)s, updated_at = NOW() WHERE id = %(id)s;"
+#     data = {
+#         'id' : user_id,
+#         "first_name": request.form["first_name"],
+#         "last_name": request.form["last_name"],
+#         "email": request.form["email"]
+#     }    
+#     user = connectToMySQL("users").query_db(query,data)
+#     print(user)
+#     return redirect (f"/display_friend/{user_id}")
+
+
+#!  remove friend
+@app.route("/delete_friend/<user_num>")
+def delete(user_num):
+    query = "DELETE FROM users WHERE id = %(user_num)s;"
+    mysql = connectToMySQL("users")
+    
+    data = {
+        'user_num' : user_num
+        }
+    users = mysql.query_db(query,data)
+    print(users)
     return redirect("/")
+
+
+#!  show individual friend
+@app.route("/display_friend/<int:user_id>")
+def friend_details(user_id):
+    query = "SELECT * FROM users WHERE id = %(id)s;"
+    data = {
+        'id' : user_id
+        }
+    
+    friend = connectToMySQL("users").query_db(query,data)
+    # users = mysql.query_db(query,data)
+
+    return render_template("friend.html", friend = friend)
+
+
+#!  edit friends page
+@app.route("/edit_friend/<int:user_id>")
+def edit_friend(user_id):
+    query = "SELECT * FROM users WHERE id = %(id)s;"
+    data = {
+        'id' : user_id
+        }
+    
+    friend = connectToMySQL("users").query_db(query,data)
+    return render_template("edit_friend.html", friend = friend[0])
+
+
+#!  update friend
+@app.route("/update_friend/<int:user_id>", methods = ['POST'])
+def update(user_id):
+    query = "UPDATE users SET first_name = %(first_name)s , last_name = %(last_name)s, email = %(email)s, updated_at = NOW() WHERE id = %(id)s;"
+    data = {
+        'id' : user_id,
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"]
+    }    
+    user = connectToMySQL("users").query_db(query,data)
+    print(user)
+    return redirect (f"/display_friend/{user_id}")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# @app.route("/remove_friend")
+# def remove_friend(id):
+#     mysql = connectToMySQL("users")
+
+#     query = "REMOVE FROM users WHERE id = [0]"
+
+#     return redirect("/")
+
 
